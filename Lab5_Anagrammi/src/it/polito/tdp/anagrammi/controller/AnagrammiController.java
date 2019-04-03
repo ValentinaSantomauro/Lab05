d33988037bdd976074030da7bccb9c1d97216469
Valentina <Valentina@172.21.14.92>
2019-04-03 14:12:03 +0200

    Ricorsione giusta. Manca il resto.

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
@@ -1,8 +1,80 @@
 package it.polito.tdp.anagrammi.controller;
 
+import java.net.URL;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+
+import it.polito.tdp.anagrammi.model.AnagrammiModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AnagrammiController {
 
-	
-}
+    @FXML
+    private ResourceBundle resources;
 
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private Button btnCalcola;
+
+    @FXML
+    private TextArea txtCorretti;
+
+    @FXML
+    private TextArea txtErrati;
+
+    @FXML
+    private Button btnReset;
+
+    AnagrammiModel m;
+
+    Set<String> anagrammi;
+    
+    public void setModel(AnagrammiModel model) {
+    	this.m=model;
+    }
+    @FXML
+    void calcolaAnagrammi(ActionEvent event) {
+    	txtCorretti.clear();
+    	txtErrati.clear();
+    	
+    	String parola=txtParola.getText();
+    	
+    	anagrammi=this.m.calcolaAnagrammi(parola);
+    	for(String s: anagrammi) {
+    	if(m.isCorrect(parola))
+    	txtCorretti.appendText(s + "\n");
+    	else
+    	txtErrati.appendText(s + "\n");
+    	}
+    	
+    }
+
+    @FXML
+    void onReset(ActionEvent event) {
+    	txtParola.clear();
+    	txtCorretti.clear();
+    	txtErrati.clear();
+    	
+    }
+
+    @FXML
+    void initialize() {
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtCorretti != null : "fx:id=\"txtCorretti\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtErrati != null : "fx:id=\"txtErrati\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+
+    }
+}
